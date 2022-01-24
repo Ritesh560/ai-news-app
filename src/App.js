832c682967784527dd6e55378dcde4b51a45a2b8
Ritesh Kumar <singlaritesh007@gmail.com>
2022-01-24 13:09:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react"
+import alanBtn from "@alan-ai/alan-sdk-web"
+
+const alanKey = "3ee4c7e8b857d6d8f57cc9d116c3c4952e956eca572e1d8b807a3e2338fdd0dc/stage"
+function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          alert("HI")
+        }
+      },
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Hello dear</h1>
+    </div>
+  )
+}
+
+export default App
